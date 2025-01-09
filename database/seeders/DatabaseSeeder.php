ea88f3bbdefe68f6ce0a7e5b0d67ccf2c4b3853b
rizal-203040067 <rizal.baihaqi007@gmail.com>
2025-01-09 12:00:54 +0700

    test change devices

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,10 @@ class DatabaseSeeder extends Seeder
         $this->call(RolesAndPermissionsSeeder::class);
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => 'admin123'
+        ]);
     }
 }
