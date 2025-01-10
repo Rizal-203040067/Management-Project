22a23b82cf9d0856eb1d74537f29715104ca055b
Rizal-203040067 <rizal.baihaqi007@gmail.com>
2025-01-10 22:29:00 +0700

    adding style to table transaction

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -27,7 +27,7 @@ class TransactionResource extends Resource
                     ->required()
                     ->maxLength(255),
                 Forms\Components\Select::make('category_id')
-                    ->relationship('categories', 'name')
+                    ->relationship('category', 'name')
                     ->required(),
                 Forms\Components\DatePicker::make('date')
                     ->required(),
@@ -47,23 +47,25 @@ class TransactionResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('name')
-                    ->searchable(),
-                Tables\Columns\IconColumn::make('category.is_expense')
-                    ->label('Expense')
-                    ->boolean(),
-                Tables\Columns\TextColumn::make('category.name')
-                    ->numeric()
+                Tables\Columns\ImageColumn::make('category.image')
                     ->sortable(),
+                Tables\Columns\TextColumn::make('category.name')
+                    ->description(fn (Transaction $record): string => $record->name)
+                    ->label('Transaction'),
+                Tables\Columns\IconColumn::make('category.is_expense')
+                    ->label('Finance')
+                    ->trueIcon('Heroicon-o-arrow-up-circle')
+                    ->falseIcon('Heroicon-o-arrow-down-circle')
+                    ->trueColor('danger')
+                    ->falseColor('success')
+                    ->boolean(),
                 Tables\Columns\TextColumn::make('date')
-                    ->date()
+                    ->label('Date')
                     ->sortable(),
                 Tables\Columns\TextColumn::make('amount')
                     ->numeric()
+                    ->money('IDR', locale:'id')
                     ->sortable(),
-                Tables\Columns\TextColumn::make('note')
-                    ->searchable(),
-                Tables\Columns\ImageColumn::make('image'),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
